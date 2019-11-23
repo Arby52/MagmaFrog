1fb2e64940405509412cb5aabed3cd213a4c5862
Ben Ritson <32337439+Arby52@users.noreply.github.com>
2019-11-23 20:15:01 +0000

    Working on background spawning

--- MagmaFrog/MagmaFrog/GameScene.swift
+++ MagmaFrog/MagmaFrog/GameScene.swift
@@ -17,10 +17,16 @@ enum CollisionType: UInt32{
     case obstacleBoulder = 16 //Stationary Obstacle Boulders
 }
 
+enum BackgroundTypes : Int{
+    case spawn = 0
+    case safe = 1
+    case magma = 2
+    case boulder = 3
+}
+
 class GameScene: SKScene {
     let player = SKSpriteNode(imageNamed: "frog")
-    let rockbg = SKSpriteNode(imageNamed: "rockbg")
-    
+        
     var isPlayerAlive = true
     var score = 0
     
@@ -28,16 +34,10 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         
-        rockbg.name = "rockbg"
-        rockbg.position.x = 0
-        rockbg.zPosition = 0
-        rockbg.position.y = 0
-        addChild(rockbg)
-        
         player.name = "player"
         player.position.x = 0  //frame.minX to get left side
         player.zPosition = 1
-        player.position.y = 0
+        player.position.y = frame.minY + 96
         addChild(player)
         
         player.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
@@ -45,6 +45,45 @@ class GameScene: SKScene {
         player.physicsBody?.collisionBitMask = CollisionType.rollingBoulder.rawValue | CollisionType.obstacleBoulder.rawValue //What can the player collide with? The | adds the two values from the enum together
         player.physicsBody?.contactTestBitMask = CollisionType.rollingBoulder.rawValue | CollisionType.obstacleBoulder.rawValue //sends a message when collisions happen
         player.physicsBody?.isDynamic = false //remove gravity
+        
+        
+        //spawn starting background
+        let currentBlocks: Int = 0
+        let neededBlocks: Int = 21
+        let prevBg: BackgroundTypes
+        
+        //starting area
+        for n in 0...2{
+            let bg = SKSpriteNode(imageNamed: "rockbg")
+            bg.name = "rockbg"
+            bg.position.x = 0
+            bg.zPosition = 0
+            bg.position.y = CGFloat((frame.minY + 32) + CGFloat(n*64))
+            addChild(bg)
+        }
+        
+        while currentBlocks < neededBlocks{
+            let bgtype = Int.random(in: 1...3)
+            switch bgtype{
+            case 1:
+                
+                break
+                
+            case 2:
+                
+                break
+                
+            case 3:
+                
+                break
+            }
+            
+        }
+        
+        //boulder spawn timer
+        var boulderSpawnTimer: Timer?
+        boulderSpawnTimer = Timer.scheduledTimer(timeInterval: 2.5, target: self, selector: #selector(SpawnBoulder), userInfo: nil, repeats: true)
+              
     }
     
     override func update(_ currentTime: TimeInterval) {
@@ -55,16 +94,6 @@ class GameScene: SKScene {
                 }
             }
         }
-        
-        //change to a timer
-        var boulderSpawnTimer: Timer?
-        boulderSpawnTimer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(SpawnBoulder), userInfo: nil, repeats: true)
-        
-        //let activeBoulders = children.compactMap{$0 as? BoulderNode}
-        //if activeBoulders.isEmpty{
-        //    SpawnBoulder()
-       // }
-        
     }
     
     @objc func SpawnBoulder(){
